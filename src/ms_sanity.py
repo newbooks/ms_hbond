009c2f840443f512e8cda8f6ca45bd34a5cf35f4
newbooks <10424666+newbooks@users.noreply.github.com>
2026-01-19 20:13:16 -0500

    closes #18

---
+++ src/ms_sanity.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import sys
+import argparse
+import pandas as pd
+import re
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Microstate Sanity Check")
+    parser.add_argument("file", type=str, help="Path to H bond count file")
+    parser.add_argument("-p", type=float, default=7.0, help="Microstate pH value (default: 7.0)")
+    args = parser.parse_args()
+
+    # Read in the H bond count file
+    if args.file.endswith(".txt"):
+        with open(args.file) as f:
+            first_line = f.readline()
+        match = re.search(r"Total microstate count:\s*(\d+)", first_line)
+        total_microstates = int(match.group(1))
+        df = pd.read_csv(args.file, sep=r"\s+", comment="#")  # ignore comment lines
+        # Get dornor and acceptor pairs and their counts
+        donor_acceptor_pairs = df[["Donor_ConfID", "Acceptor_ConfID"]].values
+        counts = df["Count"].values
+        fractions = counts / total_microstates
+    elif args.file.endswith(".csv"):
+        df = pd.read_csv(args.file)
+        # Get dornor and acceptor pairs and their counts
+        donor_acceptor_pairs = df[["donor", "acceptor"]].values
+        fractions = df["ms_occ"].values
+    else:
+        print("Unsupported file format. Please provide a .txt or .csv file.")
+        sys.exit(1)
+
+
+    passed = True
+    # Check against hah.txt to see if any pair is not in hah.txt
+    hbond_pairs = set()
+    fname_hah = "step2_out_hah.txt"
+    with open(fname_hah) as f:
+        next(f)  # skip header
+        for line in f:
+            parts = line.strip().split()
+            if len(parts) >= 2:
+                donor = parts[0].strip()
+                acceptor = parts[1].strip()
+                hbond_pairs.add((donor, acceptor))
+    for d, a in donor_acceptor_pairs:
+        if (str(d), str(a)) not in hbond_pairs:
+            print(f"Warning: Donor-Acceptor pair ({d}, {a}) not found in {fname_hah}")
+            passed = False
+
+    # Check against fort.38 to see if the donor - acceptor occupany is within the bounds
+    fname_fort38 = "fort.38"
+    df_fort38 = pd.read_csv(fname_fort38, sep=r"\s+")
+    fort38_dict = {}
+    for _, row in df_fort38.iterrows():
+        confid = row["ph"]
+        frac = row[f"{float(args.p):.1f}"]
+        fort38_dict[confid] = frac
+    for (d, a), frac in zip(donor_acceptor_pairs, fractions):
+        d_frac = fort38_dict.get(d, None)
+        a_frac = fort38_dict.get(a, None)
+        occ_max = min(d_frac, a_frac)
+        occ_min = max(0.0, d_frac + a_frac - 1.0)
+        if frac > occ_max + 1e-3:
+            print(f"Warning: Occupancy {frac:.4f} for pair ({d}, {a}) exceeds max bound {occ_max:.3f} obtained from {d_frac:.3f} (donor) and {a_frac:.3f} (acceptor)")
+            passed = False
+        if frac < occ_min - 1e-3:
+            print(f"Warning: Occupancy {frac:.4f} for pair ({d}, {a}) below min bound {occ_min:.3f} obtained from {d_frac:.3f} (donor) and {a_frac:.3f} (acceptor)")
+            passed = False
+    
+    if passed:
+        print("Sanity check passed: All donor-acceptor pairs are valid and occupancies are within bounds.")
+    else:
+        print("Sanity check failed: Issues found with donor-acceptor pairs or occupancies. Either the hbonding network or fort.38 file may be incorrect.")
+        
